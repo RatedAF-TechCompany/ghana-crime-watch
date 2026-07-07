2caba88eb9afbcbf510eb6d3729c181f6a2d8db5
Mohammed Basit Abdul <basitmohammed362@gmail.com>
2026-07-07 14:17:25 +0000

    Update default BASE_URL to use www subdomain

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://ghanacrimes.com';
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://www.ghanacrimes.com';
